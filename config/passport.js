4a39d38e08272f6d6b6c1c3b4f39b97f2ecd0bdd
NickVirga <nsvirga@gmail.com>
2024-10-12 19:07:22 -0400

    recommit forgot to save some changes for last commit

--- config/passport.js
+++ config/passport.js
@@ -126,7 +126,7 @@ passport.use(
     {
       clientID: process.env.TWITTER_OAUTH_CLIENT_ID,
       clientSecret: process.env.TWITTER_OAUTH_CLIENT_SECRET,
-      callbackURL: `${process.env.SERVER_BASE_URL}/api/auth/facebook/callback`,
+      callbackURL: `${process.env.SERVER_BASE_URL}/api/auth/twitter/callback`,
       scope: ['tweet.read', 'tweet.write', 'users.read']
     },
     function (accessToken, refreshToken, profile, done) {
